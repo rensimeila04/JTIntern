440e3757cf1b7f1d152d44c2786a94fa4653d6bf
Rensi Meila Yulvinata <54298132+rensimeila04@users.noreply.github.com>
2025-05-05 15:15:32 +0700

    feat(landing page): add panduan and footer

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -138,6 +138,87 @@
             </div>
         </div>
     </div>
+
+    {{-- Panduan --}}
+    <div class="bg-white h-fit w-full px-20 py-24 space-y-22">
+        <div class="space-y-4 flex flex-col items-center">
+            <div class="space-x-2 flex items-center w-fit rounded-full outline-1 outline-primary-200  px-6 py-2">
+                <i class="ph-fill ph-book-open text-primary-500 text-xl"></i>
+                <p class="text-base text-neutral-900">Panduan Singkat</p>
+            </div>
+            <p class="text-4xl text-neutral-900 font-semibold text-center">Maksimalkan Sistem Rekomendasi Magang</p>
+            <p class="text-center text-base text-neutral-500">Temukan peluang magang yang sesuai dengan minat dan
+                keahlianmu, ajukan lamaran dengan mudah, dan pantau<br>secara real-time dalam satu sistem terintegrasi.
+            </p>
+        </div>
+        {{-- show panduan --}}
+        @php
+            $steps = [
+                [
+                    'title' => 'Mulai dengan Profil',
+                    'desc' => 'Lengkapi profil untuk rekomendasi magang terbaik.',
+                ],
+                [
+                    'title' => 'Cari dan Ajukan Magang',
+                    'desc' => 'Temukan magang, ajukan lamaran, dan pantau status.',
+                ],
+                [
+                    'title' => 'Kelola dan Pantau Progres',
+                    'desc' => 'Catat aktivitas, pantau progres, dan terima evaluasi.',
+                ],
+                [
+                    'title' => 'Selesaikan Magang',
+                    'desc' => 'Tinjau evaluasi dan lengkapi administrasi magang.',
+                ],
+            ];
+        @endphp
+        <div class="grid grid-cols-4 gap-4">
+            @foreach ($steps as $item)
+                <div
+                    class="w-full h-fit bg-white outline-1 outline-neutral-200 rounded-2xl py-6 px-6 hover:bg-primary-50 hover:outline-primary-500 space-y-6">
+                    <div
+                        class="w-12 h-12 bg-white border-1 rounded-lg border-neutral-200 p-2 flex flex-col justify-center hover:!outline-none">
+                        <p class="text-xl font-semibold text-primary-500 text-center">{{ $loop->index + 1 }}</p>
+                    </div>
+                    <div class="space-y-1">
+                        <p class="text-xl text-medium text-neutral-900">{{ $item['title'] }}</p>
+                        <p class="text-normal text-base text-neutral-500">{{ $item['desc'] }}</p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+    <footer class="bg-white py-24 px-20 space-y-14">
+        <div class="flex justify-between items-center">
+            <p class="text-4xl font-semibold text-neutral-900">Mulai Magangmu<br>dan Raih Kesempatan<br>Terbaik!</p>
+            <div class="flex flex-col justify-start items-end space-y-4">
+                <p class="text-normal text-base text-neutral-500 text-end">Jelajahi peluang magang terbaik sesuai
+                    minat dan keahlianmu,<br>ajukan lamaran dengan mudah, dan
+                    pantau perkembanganmu<br>dalam satu sistem terintegrasi.</p>
+                <a href="#"
+                    class="mt-6 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-primary-500 text-white hover:bg-primary-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none transition duration-700 ease-in-out hover:scale-105 animate-fade-in-up delay-300">
+                    Mulai Sekarang
+                    <span><i class="ph ph-caret-double-right"></i></span>
+                </a>
+            </div>
+        </div>
+        <hr class="border-neutral-300">
+        <div class="flex justify-between">
+            <ul class="flex space-x-6 text-base">
+                <li><a href="#"
+                        class="text-neutral-500 font-normal text-base hover:text-primary-500 hover:font-semibold">Beranda</a></li>
+                <li><a href="#"
+                        class="text-neutral-500 font-normal text-base hover:text-primary-500 hover:font-semibold">Tentang</a></li>
+                <li><a href="#"
+                        class="text-neutral-500 font-normal text-base hover:text-primary-500 hover:font-semibold">Fitur</a></li>
+                <li><a href="#"
+                        class="text-neutral-500 font-normal text-base hover:text-primary-500 hover:font-semibold">Panduan</a></li>
+            </ul>
+            <p class="text-neutral-500 text-base font-normal">© Copyright 2025 . Kelompok 3 TI-2E . All right reserved</p>
+        </div>
+    </footer>
+
 </body>
 
 </html>
