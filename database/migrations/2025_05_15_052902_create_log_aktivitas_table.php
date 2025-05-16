9419553a38885ba86b828da309744975129dce72
Leon Shan <144356027+Holycious@users.noreply.github.com>
2025-05-16 23:28:20 +0700

    Update Migration: Log Aktivitas

--- database/migrations/2025_05_15_052902_create_log_aktivitas_table.php
+++ database/migrations/2025_05_15_052902_create_log_aktivitas_table.php
@@ -12,8 +12,29 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('log_aktivitas', function (Blueprint $table) {
-            $table->id();
+            $table->id('id_log_aktivitas');
+            $table->unsignedBigInteger('id_magang')->index();
+            $table->text('deskripsi');
+            $table->text('feedback_dospem')->nullable();
+            $table->string('nim');
+            $table->string('nama_mahasiswa');
+            $table->string('prodi');
+            $table->string('skema');
+            $table->string('tempat_industri');
+            $table->text('laporan')->nullable();
+            $table->text('sikap_penyampaian')->nullable();
+            $table->integer('nilai_komunikasi')->nullable();
+            $table->integer('nilai_pengembangan')->nullable();
+            $table->integer('nilai_kerja_sama')->nullable();
+            $table->integer('nilai_integritas')->nullable();
+            $table->integer('nilai_identifikasi_permasalahan')->nullable();
+            $table->integer('nilai_analisa_solusi_tik')->nullable();
+            $table->integer('nilai_pengembangan_implementasi_tik')->nullable();
+            $table->integer('nilai_pengujian_dokumentasi_tik')->nullable();
+            $table->date('tanggal');
             $table->timestamps();
+
+            $table->foreign('id_magang')->references('id_magang')->on('magang');
         });
     }
 
