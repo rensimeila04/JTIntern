528d3ca1db685d9bf8573bd04d564646c23c2039
Rensi Meila Yulvinata <54298132+rensimeila04@users.noreply.github.com>
2025-05-27 12:26:15 +0700

    feat(pagination): update 'Next' button text to 'Selanjutnya' for localization

--- resources/views/custom/pagination.blade.php
+++ resources/views/custom/pagination.blade.php
@@ -41,7 +41,7 @@
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
             <a href="{{ $paginator->nextPageUrl() }}" class="min-h-9.5 min-w-9.5 py-2 px-2.5 inline-flex justify-center items-center gap-x-1.5 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10" aria-label="Next">
-                <span>Next</span>
+                <span>Selanjutnya</span>
                 <svg class="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="m9 18 6-6-6-6"></path>
                 </svg>
